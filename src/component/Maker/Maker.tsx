2131b9b42c0ce5407c505d8ae88b7eafaa53edcf
yogjin <youngjin988@gmail.com>
2022-07-13 15:54:41 +0900

    style(Maker): styling using flex

--- src/component/Maker/Maker.tsx
+++ src/component/Maker/Maker.tsx
@@ -8,12 +8,28 @@ interface makerProps {}
 
 const Maker: FC<makerProps> = () => {
   return (
-    <>
+    <Container>
       <Header />
       <div>로그인</div>
       <Footer />
-    </>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  & > :nth-child(1) {
+    flex: 1 1 15%;
+  }
+  & > :nth-child(2) {
+    flex: 1 1 75%;
+  }
+  & > :nth-child(3) {
+    flex: 1 1 10%;
+  }
+`;
 
 export default Maker;
