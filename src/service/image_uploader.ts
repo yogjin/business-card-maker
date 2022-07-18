e83255a8e3feb79f4e8f0f54b1913d7805cba496
yogjin <youngjin988@gmail.com>
2022-07-18 19:47:28 +0900

    feat: Image uploader\

--- src/service/image_uploader.ts
+++ src/service/image_uploader.ts
@@ -1,8 +1,21 @@
 import { ImageUploader } from 'common/interfaces';
 
 class ImageUploaderImpl implements ImageUploader {
-  async upload() {
-    return 'file';
+  private url = process.env.REACT_APP_CLOUDINARY_URL;
+  async upload(file: File) {
+    const formData = new FormData();
+
+    formData.append('file', file);
+    formData.append('upload_preset', 'business-card-profile');
+
+    return fetch(this.url!, {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch(console.error);
   }
 }
 
