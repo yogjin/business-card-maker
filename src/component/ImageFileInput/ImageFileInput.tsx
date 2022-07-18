e4f96a7606070a6bd3b651bbb32df5605942fa25
yogjin <youngjin988@gmail.com>
2022-07-18 19:46:44 +0900

    feat: ImageFileInput Component

--- src/component/ImageFileInput/ImageFileInput.tsx
+++ src/component/ImageFileInput/ImageFileInput.tsx
@@ -1,13 +1,49 @@
 import { ImageUploader } from 'common/interfaces';
-import React from 'react';
+import React, { ChangeEvent, MouseEvent, useRef } from 'react';
 import { FC } from 'react';
+import styled from 'styled-components';
 
 interface ImageFileInputProps {
   imageUploader: ImageUploader;
+  name?: string;
+  // onFileChange?: Function;
 }
 
-const ImageFileInput: FC<ImageFileInputProps> = () => {
-  return <button>Image</button>;
+const ImageFileInput: FC<ImageFileInputProps> = ({
+  imageUploader,
+  name,
+  // onFileChange,
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleButtonClick = (e: MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+    inputRef.current?.click();
+  };
+
+  const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+
+    const uploaded = await imageUploader.upload(e.target.files[0]);
+  };
+  return (
+    <Container>
+      <Input
+        type="file"
+        accept="image/*"
+        name="file"
+        ref={inputRef}
+        onChange={handleFileChange}
+      />
+      <Button onClick={handleButtonClick}>{name || 'No file'}</Button>
+    </Container>
+  );
 };
 
+const Container = styled.div``;
+const Input = styled.input`
+  display: none;
+`;
+const Button = styled.button`
+  width: 100%;
+`;
 export default ImageFileInput;
