51a5783cf0605df2505aabc132134c2c2d0360df
yogjin <youngjin988@gmail.com>
2022-07-20 00:58:23 +0900

    feat(fireBase_realtime_db): setter, getter

--- src/service/firebase_readtime_db.ts
+++ src/service/firebase_readtime_db.ts
@@ -1,12 +1,28 @@
-import { FireBaseRealTimeDB } from './../common/interfaces';
+import { Card, FireBaseRealTimeDB } from './../common/interfaces';
 import { firebaseApp } from 'service/firebase';
-import { Database, getDatabase, set } from 'firebase/database';
+import { Database, get, getDatabase, ref, set } from 'firebase/database';
 
 export class FireBaseRealTimeDBImpl implements FireBaseRealTimeDB {
   private database: Database;
   constructor() {
     this.database = getDatabase(firebaseApp);
   }
-  set() {}
-  get() {}
+  set(userId: string, card: Card) {
+    set(ref(this.database, `users/${userId}/cards/${card.id}`), card);
+  }
+  async get(userId: string) {
+    let cards = await get(ref(this.database, `users/${userId}/cards/`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          return snapshot.val();
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    const arrCards: Card[] = [];
+    Object.keys(cards).map((key) => arrCards.push(cards[key]));
+
+    return arrCards;
+  }
 }
