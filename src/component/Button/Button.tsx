278991bb370ad74301565ef4b077ba5ff2a8f17b
yogjin <youngjin988@gmail.com>
2022-07-19 11:18:15 +0900

    feat: Add property "color" in <Button/> for styled-component custom color using props

--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -1,13 +1,27 @@
 import React, { MouseEventHandler } from 'react';
 import { FC } from 'react';
+import styled from 'styled-components';
 
 interface ButtonProps {
-  name: string;
-  handleClick: MouseEventHandler<HTMLButtonElement>;
+  name?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  color?: string;
 }
 
-const Button: FC<ButtonProps> = ({ name, handleClick }) => {
-  return <button onClick={handleClick}>{name}</button>;
+const Button: FC<ButtonProps> = ({ name, handleClick, color }) => {
+  return (
+    <StyledButton onClick={handleClick} color={color}>
+      {name}
+    </StyledButton>
+  );
 };
 
+const StyledButton = styled.button<ButtonProps>`
+  background-color: ${({ color }) => color};
+  border: 0;
+  cursor: pointer;
+  &:hover {
+    opacity: 90%;
+  }
+`;
 export default Button;
